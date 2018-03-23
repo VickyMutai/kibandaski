33935e7974e71e2f0e4dc6e3730547b471e70df2
VickyMutai <vicky.mutai96@gmail.com>
2018-03-23 20:14:56 +0300

    migrate database

--- food/models.py
+++ food/models.py
@@ -2,34 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Hotel(models.Model):
-    name = models.CharField(max_length = 30)
-    image_url = models.ImageField(upload_to='hotel/')
-    contact_details = models.CharField(max_length = 60)
-    location = models.CharField(max_length=30)
-    city = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
-
-    def save_hotel(self):
-        self.save()
-
-    def delete_hotel(self):
-        self.delete()
-
-    @classmethod
-    def search_hotel(cls,search_term):
-        hotel = cls.objects.filter(name__icontains=search_term)
-        return hotel
-
-
 class Food(models.Model):
     name = models.CharField(max_length=30)
     price = models.PositiveIntegerField(default=0)
     image_url = models.ImageField(upload_to='food/')
     available = models.PositiveIntegerField(default=0)
-    hotel = models.ForeignKey(Hotel)
 
     def __str__(self):
         return self.name
@@ -49,7 +26,6 @@ class Cart(models.Model):
     items = models.CharField(max_length=255)
     prices = models.PositiveIntegerField(default=0)
     user = models.ForeignKey(User)
-    hotel = models.ForeignKey(Hotel)
     food = models.ForeignKey(Food)
 
     def __str__(self):
