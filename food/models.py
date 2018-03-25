4dedd7f7d6612704a41040ea9391215dbc14c3c3
VickyMutai <vicky.mutai96@gmail.com>
2018-03-26 00:01:42 +0300

    making migrations

--- food/models.py
+++ food/models.py
@@ -7,10 +7,14 @@ class Category(models.Model):
 
 class Food(models.Model):
     name = models.CharField(max_length=30)
-    price = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=255)
     image_url = models.ImageField(upload_to='food/')
-    available = models.BooleanField(default=False)
-    category = models.ForeignKey(Category, null=True, blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
